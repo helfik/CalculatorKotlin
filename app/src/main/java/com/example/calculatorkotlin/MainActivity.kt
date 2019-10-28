9d37340fff95fe6d8a0b7488f8e366f5b726e28b
Nigar Kazim <niko.kazim@gmail.com>
2019-10-28 11:29:07 +0400

    fixed issue

--- app/src/main/java/com/example/calculatorkotlin/MainActivity.kt
+++ app/src/main/java/com/example/calculatorkotlin/MainActivity.kt
@@ -8,28 +8,28 @@ import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
 
-    var one = findViewById<TextView>(R.id.one)
-    var two = findViewById<TextView>(R.id.two)
-    var three = findViewById<TextView>(R.id.three)
-    var four = findViewById<TextView>(R.id.four)
-    var five = findViewById<TextView>(R.id.five)
-    var six = findViewById<TextView>(R.id.six)
-    var seven = findViewById<TextView>(R.id.seven)
-    var eight = findViewById<TextView>(R.id.eight)
-    var nine = findViewById<TextView>(R.id.nine)
-    var zero = findViewById<TextView>(R.id.zero)
-    var plus = findViewById<TextView>(R.id.plus)
-    var minus = findViewById<TextView>(R.id.minus)
-    var multiple = findViewById<TextView>(R.id.multiple)
-    var divide = findViewById<TextView>(R.id.divide)
-    var expression = findViewById<TextView>(R.id.expression)
-    var result = findViewById<TextView>(R.id.result)
-    var clear = findViewById<TextView>(R.id.clear)
-    var equal = findViewById<TextView>(R.id.equal)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val one = findViewById<TextView>(R.id.one)
+        val two = findViewById<TextView>(R.id.two)
+        val three = findViewById<TextView>(R.id.three)
+        val four = findViewById<TextView>(R.id.four)
+        val five = findViewById<TextView>(R.id.five)
+        val six = findViewById<TextView>(R.id.six)
+        val seven = findViewById<TextView>(R.id.seven)
+        val eight = findViewById<TextView>(R.id.eight)
+        val nine = findViewById<TextView>(R.id.nine)
+        val zero = findViewById<TextView>(R.id.zero)
+        val plus = findViewById<TextView>(R.id.plus)
+        val minus = findViewById<TextView>(R.id.minus)
+        val multiple = findViewById<TextView>(R.id.multiple)
+        val divide = findViewById<TextView>(R.id.divide)
+        val expression = findViewById<TextView>(R.id.expression)
+        val result = findViewById<TextView>(R.id.result)
+        val clear = findViewById<TextView>(R.id.clear)
+        val equal = findViewById<TextView>(R.id.equal)
 
         one.setOnClickListener { appendOnExpresstion("1", true) }
         two.setOnClickListener { appendOnExpresstion("2", true) }
@@ -71,6 +71,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun appendOnExpresstion(string: String, canClear: Boolean) {
+
+        val expression = findViewById<TextView>(R.id.expression)
+        val result = findViewById<TextView>(R.id.result)
 
         if(result.text.isNotEmpty()){
             expression.text = ""
